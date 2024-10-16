df7d1e7fe3d1e7ad3667e42a84b71806a8af1ff4
CesarVillagomez117 <viej080310hgtlssa6@cbtis217.edu.mx>
2024-10-16 08:37:52 -0600

    concluyendo con el codigo completo version 4

--- suma.cpp
+++ suma.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <bits/stcd++.h>
 int main (){
+    //cuarto commit codigo completo version 4
+    int a,b,r;
+    cin>>a>>b;
+    r=a+b;
+    cout<<r;
     return 0;
     //tercer commit 
+
 }
